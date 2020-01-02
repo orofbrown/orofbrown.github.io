d9f5ba72e8740cbf91b4b60cb28e19210a5171a5
Mike Young <michael.young@rallyhealth.com>
2020-01-01 21:01:23 -0600

    Adding jude game

---
+++ jude-game/precache-manifest.82daecd411b6fc3d9aac148891e4947e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb06ff2a50f104f1ad83980f5efabdd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b8154879606b865ee7",
+    "url": "/static/css/main.93281a5b.chunk.css"
+  },
+  {
+    "revision": "5f874a729d2ad4956c9c",
+    "url": "/static/js/2.2c1e7b5e.chunk.js"
+  },
+  {
+    "revision": "68b8154879606b865ee7",
+    "url": "/static/js/main.96e7d1a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86e9cc6a64e4815c9c4a24ec1f6e5b26",
+    "url": "/static/media/hydra.86e9cc6a.png"
+  },
+  {
+    "revision": "feb2a4a68fe1867403681dccbbbc4d80",
+    "url": "/static/media/jude.feb2a4a6.gif"
+  },
+  {
+    "revision": "90aa8df391323d8556125d31143fc4d9",
+    "url": "/static/media/scrapFace.90aa8df3.png"
+  },
+  {
+    "revision": "58532a1d9e82896fe2c82d1e40dbfcdb",
+    "url": "/static/media/spider-collapse.58532a1d.gif"
+  },
+  {
+    "revision": "873b160ec05ca523b0711a038f6ad7a2",
+    "url": "/static/media/stal-collapse.873b160e.gif"
+  },
+  {
+    "revision": "8088e368251a4128b6ac0d2fd5ac791c",
+    "url": "/static/media/zombie-collapse.8088e368.gif"
+  }
+]);
